379b2766ca313a9444544714b366907266a8c08b
Stephen Woosley <stephenrwoosley@gmail.com>
2018-03-08 16:42:48 -0500

    notes for further development

--- main.js
+++ main.js
@@ -11,3 +11,15 @@ button.onclick = function() {
     let currentName = names[random]
     nameDiv.innerHTML = `You're up ${currentName}!`;
 }
+
+// Get data from .json instead of names array
+
+// incorporate object properties so that I can console.log them in the browser and have access to them
+
+// play around with incrementing points
+    // will need to have an incrementing UI element
+    // will need to show the current points
+
+// update the UI with CSS Grid and Flexbox and colors from the CSS Grid Firefox Example
+
+// connect to a mongo db, and set so I can store historical point totals but refresh weekly point totals (guess that could just be two fields)
